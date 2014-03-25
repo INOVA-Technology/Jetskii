467825aaae07e3068abb9b0e54fb33485160be8f
Chase <kacmlweimer@gmail.com>
2014-03-25 18:14:09 -0500

    initial

---
+++ JetskiiTests/JetskiiTests.m
@@ -0,0 +1,34 @@
+//
+//  JetskiiTests.m
+//  JetskiiTests
+//
+//  Created by Chase on 3/23/14.
+//  Copyright (c) 2014 INOVA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JetskiiTests : XCTestCase
+
+@end
+
+@implementation JetskiiTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
